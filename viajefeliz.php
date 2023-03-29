cc4a190479a2370e23933fd0945f9b6772b832c4
BrunoFerrada <brunoferrada1212@gmail.com>
2023-03-29 18:36:35 -0300

    finalizacion de funciones

--- viajefeliz.php
+++ viajefeliz.php
@@ -27,6 +27,43 @@
         public function ponerDestino($destino) {
             $this->destino = $destino;
           }
+        
+        public function obtenerCantMaxPasajeros() {
+            return $this->cantMaxPasajeros;
+        }
 
+        public function ponerCantMaxPasajeros($cantMaxPasajeros) {
+            $this->cantMaxPasajeros = $cantMaxPasajeros;
+        }
 
+        public function obtenerPasajerosViaje() {
+            return $this->pasajerosViaje;
+        }
+
+        public function ponerPasajerosViaje($pasajerosViaje) {
+            $this->pasajerosViaje = $pasajerosViaje;
+        }
+
+       public function agregarPasajeros($pasajeros) {
+            array_push($this->pasajerosViaje, $pasajeros);
+       }
+
+       public function modificarPasajeros($numeroDocumento, $pasajeros) {
+        foreach ($this->pasajerosViaje as $key => $value) {
+            if ($value['numero de documento'] == $numeroDocumento) {
+                $this->pasajerosViaje[$key]['nombre'] = $pasajeros['nombre'];
+                $this->pasajerosViaje[$key]['apellido'] = $pasajeros['apellido'];
+                break;       
+            }
+        }
+       }
+
+       public function quitarPasajeros($numeroDocumento) {
+        foreach ($this->pasajerosViaje as $key => $value) {
+            if ($value['numero de documento'] == $numeroDocumento) {
+                unset($this->pasajerosViaje[$key]);
+                break;
+            }
+        }
+       }
     }
