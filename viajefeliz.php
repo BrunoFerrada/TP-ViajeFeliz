5653cb03a9b6252a5e3855d6d8dcfeec3756beae
BrunoFerrada <brunoferrada1212@gmail.com>
2023-03-29 17:43:15 -0300

    definiendo funciones

--- viajefeliz.php
+++ viajefeliz.php
@@ -1,2 +1,32 @@
 <?php
-    
+    class Viaje {
+        private $codigo;
+        private $destino;
+        private $cantMaxPasajeros;
+        private $pasajerosViaje;
+
+        public function __construct($codigo, $destino, $cantMaxPasajeros) {
+            $this->codigo = $codigo;
+            $this->destino = $destino;
+            $this->cantMaxPasajeros = $cantMaxPasajeros;
+            $this->pasajerosViaje = array();
+        }
+
+        public function obtenerCodigo() {
+            return $this->codigo;
+        }
+
+        public function ponerCodigo($codigo) {
+            $this->codigo = $codigo;
+        }
+
+        public function obtenerDestino() {
+            return $this->destino;
+        }
+
+        public function ponerDestino() {
+            $this->destino = $destino;
+        }
+
+
+    }
